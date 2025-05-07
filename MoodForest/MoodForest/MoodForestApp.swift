2830d5d13324e5bb33fd69487f1f258e11f3e70a
Marria Rybak <marjarybak@gmail.com>
2025-05-07 17:23:50 +0200

    Initial commit (clean history)

---
+++ MoodForest/MoodForest/MoodForestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodForestApp.swift
+//  MoodForest
+//
+//  Created by Mariia Rybak on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodForestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodForest/MoodForestTests/MoodForestTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodForestTests.swift
+//  MoodForestTests
+//
+//  Created by Mariia Rybak on 07.05.2025.
+//
+
+import Testing
+@testable import MoodForest
+
+struct MoodForestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
